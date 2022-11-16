23237aaa23c283b0328126cb5281fc94b8204e9f
vinisiq <viniciusdp2@outlook.com>
2022-11-16 09:39:36 -0300

    subindo pipeline

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.valcann.com.br/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Valcann Cloud Intelligence
         </a>
       </header>
     </div>
